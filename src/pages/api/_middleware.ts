d6b670f115d6f7a5ead81e418be889974e4f57ba
felixgro <me@felixgrohs.dev>
2022-04-09 18:45:10 +0200

    remove api middleware

--- src/pages/api/_middleware.ts
+++ src/pages/api/_middleware.ts
@@ -1,11 +1,5 @@
 import { NextFetchEvent, NextRequest } from 'next/server';
 
-const hostWhitelist = ['localhost'];
-
 export function middleware (req: NextRequest, ev: NextFetchEvent) {
-	if (!req.url.startsWith('http://')) {
-		return new Response('Not Allowed', {
-			status: 401
-		});
-	}
+	//
 }
