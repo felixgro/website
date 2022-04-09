105fdecd2e98a56d992ed2477038e157abf1c5b9
felixgro <me@felixgrohs.dev>
2022-04-09 18:47:32 +0200

    update

--- src/pages/api/_middleware.ts
+++ src/pages/api/_middleware.ts
@@ -1,13 +1,5 @@
 import { NextFetchEvent, NextRequest } from 'next/server';
 
 export function middleware (req: NextRequest, ev: NextFetchEvent) {
-<<<<<<< HEAD
-	// if (!req.url.startsWith('http://')) {
-	// 	return new Response('Not Allowed', {
-	// 		status: 401
-	// 	});
-	// }
-=======
 	//
->>>>>>> main
 }
